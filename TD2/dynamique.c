e45cfbfbf76b48aa7ff3d1fbe7915272a752042d
Anaïs <anais.andre@aol.fr>
2022-02-07 19:33:18 +0100

    07/02

--- TD2/dynamique.c
+++ TD2/dynamique.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
 int* copie(int *tab, int n) {
-    int tab2[n];
+    
+    int *tab2= malloc(sizeof(int) * n);
+    
     for (int i = 0; i < n; i++) {
         tab2[i] = tab[i];
     }
 
-    int *p;
-    p = malloc(sizeof(int) * n);
-    
-    p=&tab2;
-
-    return *p;
+    return tab2;
 }
 // message d'alerte: la fonction copie retourne une adresse d'une variable locale (adresse qui disparait apres l'utilisation de la fonction car sauvegardée dans le stockage automatique (pile) )
+
+int* unsurdeux(int *tab, int n){
+    int *tab2=malloc(sizeof(int) * n/2);
+    int j=0;
+
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            tab2[j]=tab[i]
+            j++;
+        }
+    }
+
+    return tab2;
+}
+
+int main(){
+
+    int tab[3]={1,2,3};
+    printf("%lu", (long unsigned) unsurdeux(tab,3));
+    free(tab2);
+}
+
